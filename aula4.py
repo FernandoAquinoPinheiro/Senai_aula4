c6d22292afd41156bafe4e053b018a1c9fee5297
FernandoAquinoPinheiro <144848529+FernandoAquinoPinheiro@users.noreply.github.com>
2023-09-15 16:46:25 -0300

    Create aula4.py

---
+++ aula4.py
@@ -0,0 +1,60 @@
+# n1 =10
+
+# if n1 ==10 or n1 >11:
+#     print(ok)
+# # Sinais de comparação
+
+# if n1 > 1 or n1>=2:
+#     print("ok")
+
+# # and = 2 expressões precisa, a ser verdadeiras
+
+# if n1> 1 or n1 >=2:
+#     print("maior")
+
+# # or = tanto faz, qualquer uma delas, podem ser vedadeiras
+
+# if not n1 == 1:
+#     print ("não é igual")
+
+#     # não é igual == not precisa ser falsa para ser verdadeira
+
+# concatenar
+
+# n1 = 2
+# n2 = 12
+# #  textox e numeros
+
+# print(n1,n2)
+
+# # textos e numeros
+
+# print(f'{n2}{n1}')
+
+# # com numeros reais
+
+# print("hello %s %d"%(n1,n2))
+
+# #  com numeros reais e textos
+
+# print("hello {}{}".format(n1,n2))
+
+# # condicional simples
+# # if n1 >10:
+#     # print("é maior")
+
+#     # composta
+#     # else
+# n1 =  int(input("Digite o primeiro número "))
+# n2 = int(input("Digite outro número "))
+
+# opcao = "a"
+# opcao2 = "b"
+# opcao3 = "c"
+
+# if opcao == "b":
+#    print(n1+n2)
+# elif opcao2 == "a":
+#    print(n1*n2)     
+# elif opcao3 == "c":
+#    print(n1-n2)
